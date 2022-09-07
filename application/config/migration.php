4aa89949a8ac49a0b1162d0df3d8b3cbe40eca77
C. Eduardo Carrasco <educamo@gmail.com>
2022-09-07 19:06:59 -0400

    migrate de triggers de insert y update clientes al modificar facturas

--- application/config/migration.php
+++ application/config/migration.php
@@ -69,7 +69,7 @@ $config['migration_auto_latest'] = TRUE;
 | be upgraded / downgraded to.
 |
 */
-$config['migration_version'] = 20220907130601;
+$config['migration_version'] = 20220907184250;
 
 /*
 |--------------------------------------------------------------------------
