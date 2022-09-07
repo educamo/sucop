76228f736333a18abd329d498c98f721542f3eae
C. Eduardo Carrasco <educamo@gmail.com>
2022-09-07 18:33:05 -0400

    se creo migrate agrega campos totalFact y totalDeuda en tabla clientes

--- application/config/migration.php
+++ application/config/migration.php
@@ -69,7 +69,7 @@ $config['migration_auto_latest'] = TRUE;
 | be upgraded / downgraded to.
 |
 */
-$config['migration_version'] = 20220906131101;
+$config['migration_version'] = 20220907130601;
 
 /*
 |--------------------------------------------------------------------------
